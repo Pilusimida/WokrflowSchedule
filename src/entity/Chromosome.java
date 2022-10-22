1cea075654357273ee18f75d544bc450b1512615
LinJie <1249706509@qq.com>
2022-10-22 23:27:52 +0800

    添加基因库

--- src/entity/Chromosome.java
+++ src/entity/Chromosome.java
@@ -1,5 +1,8 @@
 package entity;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * This class is entity.Chromosome in EMO, which has 3 Strings:\n
  * 1. Order: a vector containing a permutation of all task indexes.
@@ -10,6 +13,12 @@ public class Chromosome implements Cloneable{
     public int[] order;
     public int[] task2ins;
     public int[] ins2type;
+
+    public List<Chromosome> better = new LinkedList<>();
+    public List<Chromosome> worse = new LinkedList<>();
+
+    public double makespan;
+    public double cost;
     public Chromosome(int[] Order, int[] Ins, int[] Type){
         this.order = Order;
         this.task2ins = Ins;
--- src/service/PopulationManager.java
+++ src/service/PopulationManager.java
@@ -2,21 +2,23 @@ package service;
 
 import entity.Chromosome;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class PopulationManager {
 
     private static int quantity;
     private static int generation;
+
+    private static double mutateProbability;
     private static List<Chromosome> bank = new ArrayList<>();
+    private static List<List<Chromosome>> rank = new ArrayList<>();
 
     static {
         ResourceBundle bundle=ResourceBundle.getBundle("config");
         quantity = Integer.parseInt(bundle.getString("evolution.population"));
         generation = Integer.parseInt(bundle.getString("evolution.generation"));
+        mutateProbability = Double.parseDouble(bundle.getString("evolution.mutate"));
+
     }
 
     public static void initialPopulation(){
@@ -39,7 +41,66 @@ public class PopulationManager {
         }
     }
 
+    public static void evaluate(){
+        bank.forEach(x->{
+            x.makespan=Agent.makespan(x);
+            x.cost = Agent.cost(x);
+        });
+    }
+    public static void reproduce(){
+        List<Chromosome> newBank=new LinkedList<>(bank);
+        for(int i=0;i<newBank.size();++i){
+            for(int j=i+1;j<newBank.size();++j){
+                Agent.crossoverOrder(newBank.get(i),newBank.get(j));
+                Agent.crossoverIns(newBank.get(i),newBank.get(j));
+                if(isMutate()) {
+                    Agent.mutateOrder(newBank.get(i),new Random().nextInt(newBank.get(i).order.length));
+                    Agent.mutateIns(newBank.get(i));
+                }
+            }
+        }
+        bank.addAll(newBank);
+    }
 
+    public static void sort(){
+        for(int i=0;i<bank.size();++i){
+            Chromosome chromosome=bank.get(i);
+            for(int j=i+1;j<bank.size();++j){
+                Chromosome temp=bank.get(j);
+                if(chromosome.makespan<temp.makespan&&chromosome.cost<temp.cost){
+                    chromosome.worse.add(temp);
+                    temp.better.add(chromosome);
+                }
+                if(chromosome.makespan>temp.makespan&&chromosome.cost>temp.cost){
+                    chromosome.better.add(temp);
+                    temp.worse.add(chromosome);
+                }
+            }
+        }
+        List<Chromosome> newBank=new LinkedList<>(bank);
+        int r = 0;
+        while (!newBank.isEmpty()) {
+            rank.add(new LinkedList<>());
+            for (Chromosome chromosome : newBank) {
+                if (chromosome.better.size() == 0) {
+                    newBank.remove(chromosome);
+                    rank.get(r).add(chromosome);
+                }
+            }
+            for(Chromosome chromosome:rank.get(r)){
+                for(Chromosome worse:chromosome.worse){
+                    worse.better.remove(chromosome);
+                }
+            }
+            r++;
+        }
+    }
+
+    public static boolean isMutate(){
+        Random random=new Random();
+        int standard = random.nextInt(10000);
+        return mutateProbability * 10000 > standard;
+    }
 
 
 
